76d189ef9a2108df06fb5cbfb3c461194d71ba63
Kuribo50 <martin.beroiza0@gmail.com>
2025-01-05 04:42:20 -0300

    Agregar comando personalizado para crear superusuario

--- backend/management/commands/create_admin.py
+++ backend/management/commands/create_admin.py
@@ -1,17 +1,22 @@
+import os
 from django.core.management.base import BaseCommand
-from django.contrib.auth.models import User
-from django.conf import settings
+from django.contrib.auth import get_user_model
 
 class Command(BaseCommand):
-    help = 'Crea un superusuario con credenciales predeterminadas si no existe.'
+    help = 'Crea un superusuario utilizando variables de entorno'
 
     def handle(self, *args, **kwargs):
-        username = 'admin'
-        password = 'admin'  # **Cambia esto a una contraseña más segura en producción**
-        email = ''
+        User = get_user_model()
+        username = os.environ.get('admin')
+        email = os.environ.get('DJANGO_SUPERUSER_EMAIL', '')
+        password = os.environ.get('admin')
+
+        if not username or not password:
+            self.stdout.write(self.style.ERROR('Las variables DJANGO_SUPERUSER_USERNAME y DJANGO_SUPERUSER_PASSWORD son necesarias'))
+            return
 
         if not User.objects.filter(username=username).exists():
-            User.objects.create_superuser(username=username, password=password, email=email)
+            User.objects.create_superuser(username=username, email=email, password=password)
             self.stdout.write(self.style.SUCCESS(f'Superusuario "{username}" creado exitosamente.'))
         else:
             self.stdout.write(self.style.WARNING(f'El superusuario "{username}" ya existe.'))
